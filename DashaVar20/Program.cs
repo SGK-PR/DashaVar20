06365dea4c6f12363ec2ec2dae2c534907639653
Ryzhkovy <sgk-pr@mail.ru>
2020-07-03 16:35:44 +0300

    Добавьте файлы проекта.

---
+++ DashaVar20/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DashaVar20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Введите код специальности:");
+            s = Convert.ToString(Console.ReadLine());
+
+            string[] vs = s.Split('-');
+            if(s.Length<9 && s.Length>7)
+            {
+                Console.WriteLine("Код верный");
+            }
+            else 
+            {
+                Console.WriteLine("Код неверный");
+            }
+            Console.ReadLine();
+        }
+         
+      
+        
+    }
+}
